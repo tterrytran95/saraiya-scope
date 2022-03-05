e54531e5c85b89a23eca4fc53eb8858f26ce2801
terry <tterrytran95@gmail.com>
2022-03-05 22:29:06 +0000

    initial commit

---
+++ state.py
@@ -0,0 +1,15 @@
+
+END = "" # terminating event 
+FORWARD = "" # forward event 
+BACKWARD = "" # backward event 
+
+class State:
+    
+    def __init__(self, current=None, previous=None):
+        self.current = current
+        self.previous = previous 
+        self.museum = museum # list of frames that represent the museum
+    
+    def start():
+        while current != 
+
