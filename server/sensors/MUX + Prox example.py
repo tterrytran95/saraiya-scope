73c425c26ab44e003e0d34c49951b8cf3b1f1bdc
Terry <tterrytran95@gmail.com>
2022-07-09 13:25:16 -0700

    defining states

--- server/sensors/MUX + Prox example.py
+++ server/sensors/MUX + Prox example.py
@@ -6,6 +6,17 @@ import adafruit_tca9548a
 import sys
 import adafruit_vcnl4040
 
+    
+def get_state(current, previous):
+    if current == previous:
+        return 'stable'
+    
+    if current > previous:
+        return 'forward'
+    
+    if current < previous:
+        return 'backward'
+
 # Create I2C bus as normal
 i2c = board.I2C()
 
@@ -14,18 +25,37 @@ i2c = board.I2C()
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
 prox1 = adafruit_vcnl4040.VCNL4040(tca[0])
-prox2 = adafruit_vcnl4040.VCNL4040(tca[3])
+prox2 = adafruit_vcnl4040.VCNL4040(tca[7])
 prox3 = adafruit_vcnl4040.VCNL4040(tca[4])
-prox4 = adafruit_vcnl4040.VCNL4040(tca[7])
+prox4 = adafruit_vcnl4040.VCNL4040(tca[3])
+
+current = 'prox1'
+previous = None
 
 while True:
-    p1 = prox1.proximity
-    p2 = prox2.proximity
-    p3 = prox3.proximity
-    p4 = prox4.proximity
+    previous = current 
+    max_value = -1
     
-    print('prox1:', p1)
-    print('prox2:', p2)
-    print('prox3:', p3)
-    print('prox4:', p4)
-    time.sleep(3)
+    p1 = prox1.proximity
+    if p1 > max_value:
+        current = 'prox1'
+        max_value = p1
+    
+    p2 = prox2.proximity
+    if p2 > max_value:
+        current = 'prox2'
+        max_value = p2
+    
+    p3 = prox3.proximity
+    if p3 > max_value:
+        current = 'prox3'
+        max_value = p3
+        
+    p4 = prox4.proximity
+    if p4 > max_value:
+        current = 'prox4'
+        max_value = p4
+    
+    print(get_state(current, previous))
+          
+    time.sleep(1)
